346978d6c024f01c44cf88b74677fa88047ea843
Andres Pache <apache90@gmail.com>
2015-02-14 15:26:39 -0200

    Refactor hmacAuthInterceptor

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 HMAC Auth Interceptor Module
-===========================
+============================
+
+This is an Angular HTTP Interceptor for
+[HMAC Authentication](http://en.wikipedia.org/wiki/Hash-based_message_authentication_code).
+
+
--- src/hmacAuthInterceptor.js
+++ src/hmacAuthInterceptor.js
@@ -5,68 +5,60 @@
 
   .factory('hmacInterceptor',['hmacSigner', function(hmacSigner) {
 
-    var _host;
-    var _whitelist;
-    var _accessId;
-    var _secretKey;
+    var configurations = {
+      host: '',
+      whitelist: [],
+      accessId: '',
+      secretKey: '',
+      headers: {
+        contentType: 'X_HMAC_CONTENT_TYPE',
+        contentMD5: 'X_HMAC_CONTENT_MD5',
+        date: 'X_HMAC_DATE',
+        authorization: 'X_HMAC_AUTHORIZATION'
+      }
+    }
 
-    var host = function(){
-      return angular.isFunction(_host) ? _host() : _host;
+    var getConfig = function(field, isHeader) {
+      isHeader = typeof(isHeader) !== 'undefined' ? isHeader : false;
+      var value = isHeader ?  configurations[headers][field] : configurations[field];
+      return angular.isFunction(value) ? value() : value;
     };
 
-    var secretKey = function(){
-      return angular.isFunction(_secretKey) ? _secretKey() : _secretKey;
-    };
-
-    var accessId = function(){
-      return angular.isFunction(_accessId) ? _accessId() : _accessId;
-    };
-
-    var whitelist = function(){
-      return [].concat(angular.isFunction(_whitelist) ? _whitelist() : _whitelist);
-    };
-
-    var setHost = function(value){
-      _host = value;
-    };
-
-    var setWhitelist = function(value){
-      _whitelist = value;
-    };
-
-    var setSecretKey = function(value){
-      _secretKey = value;
-    };
-
-    var setAccessId = function(value){
-      _accessId = value;
+    var setConfig = function(field, value, isHeader) {
+      isHeader = typeof(isHeader) !== 'undefined' ? isHeader : false;
+      isHeader ? configurations[headers][field] = value : configurations[field] = value;
     };
 
     var request = function(config){
-      if(config.url.search(host()) > -1) {
+      if(config.url.search(getConfig('host')) > -1) {
 
         var isWhitelist = false;
-        var whitelistArray = whitelist();
-        var whitelistLength = whitelistArray.length;
+        var whitelistArray = [].concat(getConfig('whitelist'));
 
-        for (var i = 0; i < whitelistLength; i++) {
+        for (var i = 0; i < whitelistArray.length; i++) {
           if(config.url.search(whitelistArray[i]) > -1){
             isWhitelist = true;
             break;
           }
         }
 
-        if(!isWhitelist) hmacSigner.sign(config, accessId(), secretKey());
+        if(!isWhitelist) hmacSigner.sign(config, getConfig('accessId'), getConfig('secretKey'), configurations.headers);
       }
 
       return config;
     };
 
     return {
-      setHost: setHost,
-      setWhitelist: setWhitelist,
-      setAccessId: setAccessId,
-      setSecretKey: setSecretKey,
+      set host(v) { setConfig('host', v) },
+      set whitelist(v) { setConfig('whitelist', v) },
+      set accessId(v) { setConfig('accessId', v) },
+      set secretKey(v) { setConfig('secretKeyt', v) },
+      headers: {
+         set contentType(v) { setConfig('contentType', v, true) },
+         set contentMD5(v) { setConfig('contentMD5', v, true) },
+         set date(v) { setConfig('date', v, true) },
+         set authorization(v) { setConfig('authorization', v, true) }
+      },
       request: request
     };
 
@@ -74,43 +66,64 @@
 
   angular.module('hmacAuthInterceptorSigner',[]).factory('hmacSigner', function() {
 
-    var sign = function(request, accessId, secretKey){
-      var headers = request.headers;
-      headers['Content-Type'] = contentType(request);
-      headers['Content-MD5'] = contentMD5(request);
-      headers['HMAC-Date'] = dateUTC();
-      headers['Authorization'] = 'APIAuth ' + accessId + ':' + signature(canonicalString(request), secretKey);
+    var sign = function(request, accessId, secretKey, headersConfig){
+      setCustomHeaders(request, headersConfig);
+      setAuthorizationHeader(request, accessId, secretKey, headersConfig);
     };
 
-    var signature = function(canonicalString, secretKey){
-      return CryptoJS.HmacSHA1(canonicalString, secretKey).toString(CryptoJS.enc.Base64);
+    var setCustomHeaders = function(request, headersConfig){
+      setHeader(request, headersConfig.contentType, contentType(request));
+      setHeader(request, headersConfig.contentMD5, contentMD5(request));
+      setHeader(request, headersConfig.date, timestamp());
     };
 
-    var canonicalString = function(request){
-      var contentType = request.headers['Content-Type'];
-      var contentMD5 = request.headers['Content-MD5'];
-      var requestURI = requestRelativeURI(request);
-      var timestamp = request.headers['HMAC-Date'];
-
-      return [contentType, contentMD5, requestURI, timestamp].join(',');
+    var setAuthorizationHeader = function(request, accessId, secretKey, headersConfig){
+      var message = canonicalString(request, headersConfig);
+      var value = 'APIAuth ' + accessId + ':' + signature(message, secretKey);
+      setHeader(request, headersConfig.authorization, value);
     };
 
-    var dateUTC = function(){
+    var signature = function(message, secretKey){
+      return CryptoJS.HmacSHA1(message, secretKey).toString(CryptoJS.enc.Base64);
+    };
+
+    var canonicalString = function(request, headersConfig){
+      return [
+        getHeader(request, headersConfig.contentType),
+        getHeader(request, headersConfig.contentMD5),
+        relativeURI(request),
+        getHeader(request, headersConfig.date)
+      ].join(',');
+    };
+
+    var timestamp = function(){
       return new Date().toUTCString();
     };
 
     var contentType = function(request){
-      return request.headers['Content-Type'].replace('utf-8', 'UTF-8');
+      return getHeader(request, 'Content-Type');
     }
 
-    var requestRelativeURI = function(request){
+    var relativeURI = function(request){
       return request.url.replace(/^(?:\/\/|[^\/]+)*/, "");
     };
 
     var contentMD5 = function(request){
-      var data = request.data;
-      data = angular.isDefined(data) ? (angular.isString(data) ? data : angular.toJson(data)) : '';
-      return CryptoJS.MD5(data).toString(CryptoJS.enc.Base64);
+      var content = body(request);
+      return CryptoJS.MD5(content).toString(CryptoJS.enc.Base64);
+    };
+
+    var body = function(request){
+      var data  = request.data;
+      return angular.isObject(data) ? angular.toJson(data) : data || '';
+    };
+
+    var getHeader = function(request, field){
+      return request.headers[field];
+    };
+
+    var setHeader = function(request, field, value){
+      return request.headers[field] = value;
     };
 
     return {
