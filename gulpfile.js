ff03eb3ba5602582398e2075f9a7b7384e495177
Andres Pache <apache90@gmail.com>
2015-02-15 16:17:35 -0200

    Add gulp, karma & jasmin Add some basic gulp taks

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strinct';
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var filesize = require('gulp-filesize');
+var jshint = require('gulp-jshint');
+var karma = require('karma').server;
+
+var paths = {
+  src: ['./src/**/*.js'],
+  tests: ['./tests/**/*Spec.js']
+};
+
+gulp.task('build', function() {
+  return gulp.src(paths.src)
+   .pipe(concat('hmacAuthInterceptor.js'))
+   .pipe(gulp.dest('dist'))
+   .pipe(filesize())
+   .pipe(uglify())
+   .pipe(rename('hmacAuthInterceptor.min.js'))
+   .pipe(gulp.dest('dist'))
+   .pipe(filesize())
+   .on('error', gutil.log)
+});
+
+gulp.task('lint', function() {
+  return gulp.src(paths.src)
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('test', function (done) {
+  karma.start({ configFile: __dirname + '/karma.conf.js', singleRun: true }, function(exitCode) {
+    process.exit(exitCode);
+  });
+});
